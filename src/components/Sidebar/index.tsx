fe8fe126edc2379e4bd1cee492e98d1dd36811ab
Maik Broisler <maikbroislerdevs@gmail.com>
2024-02-19 08:49:17 -0300

    refact: component input with composition pattern

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,4 @@
 import {
-  Search,
   BarChart,
   CheckSquare,
   Flag,
@@ -8,26 +7,26 @@ import {
   Users,
   LifeBuoy,
   Cog,
+  Search,
 } from 'lucide-react'
 
 import { Logo } from './Logo'
 
 import { NavItem } from './NavItem'
 import { UsedSpaceWidget } from '../UsedSpaceWidget'
+import { Profile } from '../Profile'
+import * as Input from '../Input'
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
-
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
-          placeholder="Search"
-        />
-      </div>
-
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} to="" />
         <NavItem title="Dashboard" icon={BarChart} to="" />
@@ -36,7 +35,6 @@ export function Sidebar() {
         <NavItem title="Reporting" icon={Flag} to="" />
         <NavItem title="Users" icon={Users} to="" />
       </nav>
-
       <div className="mt-auto flex flex-col gap-6">
         <nav className="space-y-0.5">
           <NavItem title="Support" icon={LifeBuoy} to="" />
@@ -44,6 +42,10 @@ export function Sidebar() {
         </nav>
 
         <UsedSpaceWidget />
+
+        <hr className="h-px bg-zinc-200" />
+
+        <Profile />
       </div>
     </aside>
   )
