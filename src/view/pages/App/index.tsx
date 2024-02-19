1bb4c9020fcf7b25f85a7c970e7c6817a7822741
Maik Broisler <maikbroislerdevs@gmail.com>
2024-02-19 11:06:01 -0300

    feat: create tabs

--- src/view/pages/App/index.tsx
+++ src/view/pages/App/index.tsx
@@ -1,7 +1,10 @@
+import { SettingsTabs } from '../../../components/SettingsTabs'
+
 export function App() {
   return (
     <>
-      <h1 className="text-red-700">Hello World!</h1>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <SettingsTabs />
     </>
   )
 }
