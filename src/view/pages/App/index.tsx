f39756adac254dad41af664d47fae820b7744414
Maik Broisler <maikbroislerdevs@gmail.com>
2024-02-20 10:57:23 -0300

    feat: create textarea component and add on page

--- src/view/pages/App/index.tsx
+++ src/view/pages/App/index.tsx
@@ -4,6 +4,7 @@ import * as FileInput from '../../../components/Form/FileInput'
 import { Select } from '../../../components/Form/Select'
 import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 import { SelectItem } from '../../../components/Form/Select/SelectItem'
+import { Textarea } from '../../../components/Form/Textarea'
 
 export function App() {
   return (
@@ -191,9 +192,8 @@ export function App() {
                   </button>
                 </div>
               </div>
-              <textarea
+              <Textarea
                 id="bio"
-                className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
                 defaultValue="I'm a Product Designer based in Melbourne, Australia. I specialise in UX/UI design, brand strategy, and Webflow development."
               />
             </div>
