91d1b3d812e2662a53c4819bb1452de6306a5c81
Maik Broisler <maikbroislerdevs@gmail.com>
2024-02-20 10:47:30 -0300

    feat: add select components on page and create bio

--- src/view/pages/App/index.tsx
+++ src/view/pages/App/index.tsx
@@ -1,7 +1,10 @@
 import { SettingsTabs } from '../../../components/SettingsTabs'
 import * as Input from '../../../components/Input'
-import { Mail } from 'lucide-react'
 import * as FileInput from '../../../components/Form/FileInput'
+import { Select } from '../../../components/Form/Select'
+import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
+import { SelectItem } from '../../../components/Form/Select/SelectItem'
+
 export function App() {
   return (
     <>
@@ -111,7 +114,12 @@ export function App() {
             >
               Country
             </label>
-            <div></div>
+            <div>
+              <Select placeholder="Select a country...">
+                <SelectItem value="br" text="Brazil" />
+                <SelectItem value="us" text="United States" />
+              </Select>
+            </div>
           </div>
 
           <div className="grid-cols-form grid gap-3 pt-5">
@@ -121,7 +129,15 @@ export function App() {
             >
               Timezone
             </label>
-            <div></div>
+            <div>
+              <Select placeholder="Select a timezone...">
+                <SelectItem
+                  value="br"
+                  text="Pacific Standard Time (UTC-08:00)"
+                />
+                <SelectItem value="us" text="America São Paulo (UTC-03:00)" />
+              </Select>
+            </div>
           </div>
 
           <div className="grid-cols-form grid gap-3 pt-5">
@@ -132,7 +148,55 @@ export function App() {
               </span>
             </label>
 
-            <div></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-2 gap-3">
+                <Select placeholder="" defaultValue="normal">
+                  <SelectItem value="normal" text="Normal Text" />
+                  <SelectItem value="md" defaultChecked text="Markdown" />
+                </Select>
+
+                <div className="flex items-center gap-1">
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <Bold className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <Italic className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <Link className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <List className="h-4 w-4 text-zinc-500" strokeWidth={3} />
+                  </button>
+                  <button
+                    type="button"
+                    className="rounded-md p-2 hover:bg-zinc-50"
+                  >
+                    <ListOrdered
+                      className="h-4 w-4 text-zinc-500"
+                      strokeWidth={3}
+                    />
+                  </button>
+                </div>
+              </div>
+              <textarea
+                id="bio"
+                className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+                defaultValue="I'm a Product Designer based in Melbourne, Australia. I specialise in UX/UI design, brand strategy, and Webflow development."
+              />
+            </div>
           </div>
 
           <div className="grid-cols-form grid gap-3 pt-5">
