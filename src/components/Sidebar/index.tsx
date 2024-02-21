6d959cb672451e619a389bcb6dd378f73a57b40d
Maik Broisler <maikbroislerdevs@gmail.com>
2024-02-21 16:26:43 -0300

    style: config dark theme sidebar and add button theme switch

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -18,11 +18,13 @@ import { UsedSpaceWidget } from '../UsedSpaceWidget'
 import { Profile } from '../Profile'
 import * as Input from '../Input'
 import * as Collapsible from '@radix-ui/react-collapsible'
+
 import { Button } from '../Button'
+import { ThemeSwitch } from '../ThemeSwitch'
 
 export function Sidebar() {
   return (
-    <Collapsible.Root className="fixed  left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4  data-[state=open]:bottom-0 data-[state=open]:h-screen lg:right-auto  lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0 lg:data-[state=closed]:h-screen dark:border-zinc-800 dark:bg-zinc-900">
+    <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4  data-[state=open]:bottom-0 data-[state=open]:h-screen lg:right-auto  lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0 lg:data-[state=closed]:h-screen dark:border-zinc-800 dark:bg-zinc-900">
       <div className="flex items-center justify-between">
         <Logo />
         <Collapsible.Trigger asChild className="lg:hidden">
@@ -30,6 +32,8 @@ export function Sidebar() {
             <Menu className="h-6 w-6" />
           </Button>
         </Collapsible.Trigger>
+
+        <ThemeSwitch />
       </div>
       <Collapsible.Content
         forceMount
@@ -57,7 +61,7 @@ export function Sidebar() {
 
           <UsedSpaceWidget />
 
-          <hr className="h-px bg-zinc-200" />
+          <div className="h-px bg-zinc-200 dark:bg-zinc-700" />
 
           <Profile />
         </div>
