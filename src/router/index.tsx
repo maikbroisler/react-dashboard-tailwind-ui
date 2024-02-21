00b6649eb0bb5c3643518d48d1f64e0cff773f79
Maik Broisler <maikbroislerdevs@gmail.com>
2024-02-21 16:25:27 -0300

    feat: config theme provider

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import { App } from '../view/pages/App'
 import { AppLayout } from '../view/_layout/app.layout'
+import { ThemeProvider } from '../providers/theme-provider'
 
 export function Router() {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route path="/" element={<App />} />
-      </Route>
-    </Routes>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<App />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   )
 }
